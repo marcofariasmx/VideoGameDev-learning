f5a9dfdfa9290da03c48c10aee1f8dc78f82337a
marbrocu <marbrocumail.com>
2020-04-01 17:22:49 -0600

    encestando1

---
+++ Encestando1/src/videogame/Player.java
@@ -0,0 +1,219 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videogame;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author antoniomejorado
+ */
+public class Player extends Item{
+
+    private int direction;
+    private int width;
+    private int height;
+    private Game game;
+    private boolean grabbed;
+    private boolean released;
+    private int offsetX;
+    private int offsetY;
+    private double alpha;
+    private double v0X;
+    private double v0Y;
+    private int potentialElastic;
+    private boolean tiroParabolicoActivado;
+    double tiempoTiroParab; //tiempo para contar, estará basado en los frames x segundo (50 frames).
+    int velocidadTiro;
+    int gradosTiro;
+    int x0;
+    int y0;
+    
+    public Player(int x, int y, int direction, int width, int height, Game game) {
+        super(x, y);
+        v0X=0;
+        v0Y=0;
+        potentialElastic=0;
+        this.direction = direction;
+        this.width = width;
+        this.height = height;
+        this.game = game;
+        grabbed = false;
+        released= false;
+        tiroParabolicoActivado = false;
+        velocidadTiro = 1;
+        gradosTiro = 45;
+        tiempoTiroParab = 0;
+        x0 = 0;
+        y0 = 0;
+    }
+    public double getPotential(){
+        return potentialElastic;
+    }
+    
+    public void setVelocityX(double velocity){
+        v0X=velocity;
+    }
+    
+    public double getVelocityX(){
+        return v0X;
+    }
+    
+    public double getVelocityY(){
+        return v0Y;
+    }
+    
+    public void setVelocityY(double velocity){
+        v0Y=velocity;
+    }
+    
+    public void setGrabbed(boolean grabbed) {
+        this.grabbed = grabbed;
+    }
+    public void setReleased(boolean grabbed) {
+        this.released = grabbed;
+    }
+     public boolean isGrabbed() {
+        return grabbed;
+    }
+     public boolean isReleaded(){
+         return released;
+     }
+
+    public int getOffsetX() {
+        return offsetX;
+    }
+    
+    public int getOffsetY() {
+        return offsetY;
+    }
+    
+    public void setOffsetX(int offsetX) {
+        this.offsetX = offsetX;
+    }
+    
+    public void setOffsetY(int offsetY) {
+        this.offsetY = offsetY;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+
+    @Override
+    public void tick() {
+        if (game.getMouseManager().isIzquierdo()) {
+            if (grabbed) {
+                setX(game.getMouseManager().getX() - getOffsetX());
+                setY(game.getMouseManager().getY() - getOffsetY());
+                if((Math.abs(x)+game.getWidth()*1/4)>(Math.abs(y)+getHeight()/2-50))
+                {
+                    alpha=(Math.atan(((game.getHeight()/2-50)-Math.abs(y))/(game.getWidth()*1/4)-Math.abs(x)));
+                }
+                else{
+                    alpha=(Math.atan((game.getWidth()*1/4)-Math.abs(x))/((game.getHeight()/2-50)-Math.abs(y)));
+                }
+                
+            }
+        }
+        if(released){
+                potentialElastic=(1/2*(1*(game.getWidth()*1/4)-getX())*(game.getWidth()*1/4)-getX());
+                setVelocityX((Math.sqrt(2*potentialElastic/1))*Math.cos(alpha));
+                setVelocityY((Math.sqrt(2*potentialElastic/1))*Math.sin(alpha));
+                System.out.println(v0X);
+                System.out.println(v0Y);
+                setReleased(false);
+        }
+        if (getX() < (game.getWidth() / 4)){
+            System.out.println("Estamos en ReadyToGoArea");
+            
+            //Activamos el tiro parabólico del personaje
+            if(!grabbed){ //cuando lo suelten
+                //x=v0·cosθ·t
+                //y=v0·senθ·t-gt2/2
+                tiroParabolicoActivado = true;
+                
+            }
+            
+            
+        }
+        else{
+            System.out.println("Fuera de ReadyToGoArea");
+        }
+        if(tiroParabolicoActivado){
+            
+            //x = x0 + v0x * t
+            //y = y0 + v0y*t - (g*t^2)/2
+            
+            //Movimiento en X
+            x0 = getX();
+            setX((int) (x0 + (v0X * tiempoTiroParab)));            
+            
+            //Movimiento en Y
+            y0 = getY();
+            setY((int) (y0 - (v0Y*tiempoTiroParab - (4.9 * tiempoTiroParab * tiempoTiroParab))));
+            
+            //Ecuaciones
+            https://www.fisicalab.com/apartado/movimiento-parabolico
+            
+            //Actualizamos tiempo
+            tiempoTiroParab = tiempoTiroParab + .02;
+            System.out.println("TiroPaACTIV");
+                        
+        }
+        else{ //reseteamos los parámetros de tiro parabólico.
+            tiempoTiroParab = 0;
+        }
+        // moving player depending on flags
+        
+        // reset x position and y position if colision
+        if (getX() + 60 >= game.getWidth()) {
+            setX(game.getWidth()/4);
+            setY(game.getHeight()/2-50);
+            tiroParabolicoActivado = false;
+        }
+        else if (getX() <= -30) {
+            setX(-30);
+            tiroParabolicoActivado = false;
+        }
+        if (getY() + 80 >= game.getHeight()) {
+            setX(game.getWidth()/4);
+            setY(game.getHeight()/2-50);
+            tiroParabolicoActivado = false;
+        }
+        else if (getY() <= -20) {
+            setX(game.getWidth()/4);
+            setY(game.getHeight()/2-50);
+            tiroParabolicoActivado = false;
+        }
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(Assets.player, getX(), getY(), getWidth(), getHeight(), null);
+    }
+}
---
+++ Encestando1/src/videogame/SoundClip.java
@@ -0,0 +1,137 @@
+
+package videogame;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.AudioSystem;
+import java.io.IOException;
+import java.net.URL;
+                                                        
+
+/**
+ *
+ * @author antoniomejorado
+ */
+public class SoundClip { 
+                                                       
+        private AudioInputStream sample;
+        private Clip clip; 
+        private boolean looping = false;
+        private int repeat = 0;
+        private String filename = "";
+        
+        public SoundClip() { 
+                                                       
+                try {  
+                                                      
+                      clip = AudioSystem.getClip();
+                }catch (LineUnavailableException e) {  
+                                                      
+                      System.out.println("Error en " + e.toString());
+                }
+        }
+        
+
+        public SoundClip(String filename) { 
+                                                       
+                this();
+                load(filename);
+        }
+
+        public void setLooping(boolean looping) {
+
+                this.looping = looping; 
+        }           
+                                             
+        public void setRepeat(int repeat) {
+
+                this.repeat = repeat;
+        }  
+                                                      
+        public void setFilename(String filename) {
+
+                this.filename = filename; 
+        }
+//Los métodos de acceso son usados para obtener los valores del objeto SoundClip.
+
+        public Clip getClip() { 
+                                                       
+                return clip; 
+        }
+                                                   
+        public boolean getLooping() { 
+                                                       
+                return looping;
+        }
+                                                      
+        public int getRepeat() {
+                                                        
+                return repeat; 
+        }
+                                                     
+        public String getFilename() { 
+                                                       
+                return filename;
+        }
+                                                    
+        private URL getURL(String filename) {
+                                                       
+                URL url = null;
+                try {
+                                                     
+                      url = this.getClass().getResource(filename);
+                }catch (Exception e) {
+                                                       
+                      System.out.println("Error en " + e.toString());
+                }
+                return url;
+        }
+
+        public boolean isLoaded() {
+                                                        
+                return (boolean)(sample != null);
+        }
+
+        public boolean load(String audiofile) { 
+                                                       
+                try {
+
+                      setFilename(audiofile);
+                      sample = AudioSystem.getAudioInputStream(getURL(filename)); 
+                      clip.open(sample); 
+                      return true;
+                } catch (IOException e) { 
+                                                       
+                      System.out.println("Error en " + e.toString());
+                      return false;
+                }catch (UnsupportedAudioFileException e) {
+                                                        
+                      System.out.println("Error en " + e.toString());
+                      return false;
+                }catch (LineUnavailableException e) {
+                                                      
+                      System.out.println("Error en " + e.toString());
+                      return false;
+                }
+        }
+
+        public void play() { 
+                                                       
+                if (!isLoaded()) 
+                    return;
+                                                        
+                clip.setFramePosition(0);
+                                                     
+                if (looping) 
+                    clip.loop(Clip.LOOP_CONTINUOUSLY);
+                else 
+                    clip.loop(repeat);
+        }
+
+        public void stop() { 
+                                                       
+                clip.stop();
+        }
+}
